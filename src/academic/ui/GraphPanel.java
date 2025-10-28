7a16549b31f092018d768fb964cd8f7cbe253d9d
Sidharth-creator <sidharths2028@cs.sjcetpalai.ac.in>
2025-10-28 06:15:46 +0530

    Update GraphPanel.java

--- src/academic/ui/GraphPanel.java
+++ src/academic/ui/GraphPanel.java
@@ -1,6 +1,6 @@
-package academic.ui;
+package ui;
 
-import academic.models.ExamSession;
+import models.ExamSession;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
